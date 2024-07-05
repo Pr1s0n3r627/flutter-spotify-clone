03b26ed8501558c6612b0b847c7af07a65e716a4
Kshitij Sahdev <kshitijsahdev5@gmail.com>
2024-07-05 17:07:14 +0530

    template

---
+++ music_app/client/lib/core/theme/app_pallete.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const cardColor = Color(0xff191970); // Midnight Blue
+  static const greenColor = Color(0xff00008B); // Dark Blue (assuming to replace green)
+  static const subtitleText = Color(0xff7851A9); // Royal Purple
+  static const inactiveBottomBarItemColor = Color(0xff4169E1); // Royal Blue
+
+  static const Color backgroundColor = Color(0xff1E90FF); // Dodger Blue
+  static const Color gradient1 = Color(0xff9370DB); // Medium Purple
+  static const Color gradient2 = Color(0xff6A5ACD); // Slate Blue
+  static const Color gradient3 = Color(0xff0000CD); // Medium Blue
+  static const Color borderColor = Color(0xff483D8B); // Dark Slate Blue
+  static const Color whiteColor = Colors.white;
+  static const Color greyColor = Colors.grey;
+  static const Color errorColor = Color(0xff8B0000); // Dark Red for error
+  static const Color transparentColor = Colors.transparent;
+
+  static const Color inactiveSeekColor = Color(0xff4682B4); // Steel Blue
+}
